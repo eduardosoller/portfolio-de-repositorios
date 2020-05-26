fd54f865b5578ede2fb74a8a935db21ac6841800
Eduardo Soller <eduardosoller@gmail.com>
2020-05-26 20:34:03 -0300

    Teste portfolio finalizado

--- src/app.js
+++ src/app.js
@@ -1,33 +1,57 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
-
+const { uuid } = require("uuidv4");
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+const { title, url, techs } = request.body
+ const repository = {
+   id: uuid(),
+   title,
+   url,
+   techs,
+   likes: 0
+ }
+repositories.push(repository)
+return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  if(repositoryIndex == -1){
+    return response.status(400).json({error : "Id não encontrado"})
+  }
+   const { title, url, techs } = request.body
+   const repository = { id, title, url, techs, likes: repositories[repositoryIndex].likes }
+  repositories[repositoryIndex] = repository
+  return response.json(repository)
+  });
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  if(repositoryIndex == -1){
+    return response.status(400).json({error : "Id não encontrado"})
+  }
+    repositories.splice(repositoryIndex, 1)
+    return response.status(204).send()  
 });
-
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
-});
-
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  if(repositoryIndex == -1){
+    return response.status(400).json({error : "Id não encontrado"})
+  }
+    repositories[repositoryIndex].likes += 1
+    return response.status(200).json(repositories[repositoryIndex])  
 });
 
 module.exports = app;
